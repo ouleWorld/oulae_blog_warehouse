02e1c0f2b012492ac5f62bf17b2b584325295a27
oulae <hcylddzz@163.com>
2023-12-06 22:29:16 +0800

    feat: 暂存一下

--- markdown/性能优化/test.js
+++ markdown/性能优化/test.js
@@ -1,35 +1,34 @@
-class PerformaceOulaeSdk {
-  constructor(obj) {
-    this.promiseCache = Promise.resolve();
-    this.sessionId = undefined;
-    this.init();
-  }
-  sdkLogInit() {
-    let _resolve = undefined;
-    let _reject = undefined;
-    this.promiseCache = new Promise((resolve, reject) => {
-      _resolve = resolve;
-      _reject = reject;
-    });
-    setTimeout(() => {
-      this.sessionId = Math.random();
-      _resolve();
-    }, 3000);
-  }
-  init() {
-    this.sdkLogInit();
+const originalFetch = window.fetch;
 
-    // TODO: 性能监控数据脚本初始化
-    // TODO: 接口请求监控脚本初始化
-    // TODO: 错误监控脚本初始化
+window.fetch = function newFetch(url, config) {
+  // 开始时间
+  const startTime = Date.now();
 
-    // TODO: 发送一个页面的pv点
-  }
-  log(...params) {
-    this.promiseCache.then(() => {
-      // 调用埋点
-      // TODO: 将埋点发送给服务端
-      console.log('成功发送一个埋点: ', params);
+  return originalFetch(url, config)
+    .then((res) => {
+      // 结束时间
+      const endTime = Date.now();
+      // 请求耗时
+      const diff = endTime - startTime;
+
+      const params = {
+        ...res,
+        ...config,
+        url,
+        startTime: startTime,
+        endTime: endTime,
+        diff,
+      };
+      console.log(
+        `请求方法: ${config.method}; 请求路径: ${url}; 请求耗时: ${diff}; \n监控参数为`,
+        params
+      );
+
+      return res;
+    })
+    .catch((err) => {
+      console.log('err: ', err);
+
+      throw err;
     });
-  }
-}
+};
